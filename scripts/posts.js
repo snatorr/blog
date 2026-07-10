416e6a06ed6401f5166531ffd50f88bf1eda0651
snator <narbaezsan@gmail.com>
2026-07-10 01:44:31 -0300

    Add files via upload

--- scripts/posts.js
+++ scripts/posts.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
             titulo: "MIREN MI CAFE :3",
             fecha: "10 de julio de 2026",
             contenido: "miren mi cafe les gusta o no diganme por ig",
-            imagen: "assets/pictures/post.heic"
+            imagen: "assets/pictures/post.jpg"
         },
         {
             titulo: "Q FRIO ASE",
