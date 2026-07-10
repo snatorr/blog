bb066d06eca31c7b7cfb688b06827f93ce259081
snator <narbaezsan@gmail.com>
2026-07-10 00:09:36 -0300

    Update posts.js

--- scripts/posts.js
+++ scripts/posts.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
             titulo: "MIREN MI CAFE :3",
             fecha: "10 de julio de 2026",
             contenido: "miren mi cafe les gusta o no diganme por ig",
-            imagen: "assets/pictures/post.heif"
+            imagen: "assets/pictures/post.heic"
         },
         {
             titulo: "Q FRIO ASE",
